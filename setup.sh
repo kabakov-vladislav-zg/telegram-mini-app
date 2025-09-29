9c32bc9529ee78ca657b89c56ffc841dfcc6d9a2
vladislav <kabakov.vladislav@adinadin.ru>
2025-09-29 08:04:28 +0400

    fix: monorepo test

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 echo "🚀 Начало настройки сервера и установки зависимостей..."
 
+sudo apt-get update
 # Установка Docker
 if ! command -v docker &> /dev/null; then
   # Add Docker's official GPG key:
-  sudo apt-get update
   sudo apt-get install ca-certificates curl
   sudo install -m 0755 -d /etc/apt/keyrings
   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
